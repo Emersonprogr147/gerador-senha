612ff301880bef75a27a99ce95c122f0ec98db29
Emerson dos Santos <emerson.santos@goesnicoladelli.net>
2024-01-22 14:24:39 -0300

    code changed added  function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-console.log("TESTE "); 
+//SELEÇÃO DE ELEMENTOS 
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generate-password");
+
+//FUNÇÕES 
+
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+};
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "(){} []=<> /.,!@#$%&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+
+    let password = " "
+    const passwordLength = 10
+
+    const generators = [
+
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    ]
+
+
+    for (i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => { 
+              const randomValue = generators[Math.floor(Math.random() * generators.length)]() ; 
+      password += randomValue  ;
+        });
+     
+    }
+    password = password.slice(0, passwordLength) ; 
+    generatePasswordElement.style.display = "block ";
+    eneratePasswordElement.querySelector("h4").innerText = password ; 
+};
+
+
+// EVENTOS 
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword( getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+        );
+});
